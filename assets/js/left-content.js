f6a0800fbe8cf49e40e13652cc8fea7fb0703aa9
muhammad.ananto <muhammad.ananto@students.amikom.ac.id>
2022-12-27 23:59:40 +0700

    add image at card

--- assets/js/left-content.js
+++ assets/js/left-content.js
@@ -1,44 +1,59 @@
 let taskz = [
     {
+        img: "background-image: url('./assets/img/abs1.jpg')",
         name: "Green House"
     },
     {
+        img: "background-image: url('./assets/img/abs2.jpg')",
         name: "Cyber Punk"
     },
     {
+        img: "background-image: url('./assets/img/abs3.webp')",
         name: "Easy Crypto"
     },
     {
+        img: "background-image: url('./assets/img/abs4.webp')",
         name: "Travel Apps"
     },
     {
+        img: "background-image: url('./assets/img/abs5.jpg')",
         name: "Landing Page"
     },
     {
+        img: "background-image: url('./assets/img/abs6.jpg')",
         name: "Landing Page 2"
     },
     {
+        img: "background-image: url('./assets/img/abs1.jpg')",
         name: "Landing Page 3"
     },
     {
+        img: "background-image: url('./assets/img/abs2.jpg')",
         name: "Market Place"
     },
     {
+        img: "background-image: url('./assets/img/abs3.webp')",
         name: "Payment Gateway"
     },
     {
+        img: "background-image: url('./assets/img/abs4.webp')",
         name: "Chat Box"
     },
     {
+        img: "background-image: url('./assets/img/abs5.jpg')",
         name: "Chat Box 2"
     },
     {
+        img: "background-image: url('./assets/img/abs6.jpg')",
         name: "Landing Page 4"
     },
     {
+        img: "background-image: url('./assets/img/abs1.jpg')",
         name: "Login Page"
     },
 ]
+
+
 
 // show total list task 
 let totalTask = taskz.length
@@ -48,9 +63,12 @@ $('.count-task').html(totalTask)
 let res = []
 taskz.map(task=>{
     if(task.name.toLocaleLowerCase().includes(text)){
-        res.push(task.name)
+        let data = {name : task.name, img:task.img}
+        res.push(data)
     }
 })
+
+console.log(res);
 
 
 
@@ -66,18 +84,18 @@ res.forEach((name)=>{
 
     if(i < 6){
         // get first letter on first word and second word
-        const [firstWord, secondWord] = name.split(' ')
+        const [firstWord, secondWord] = name.name.split(' ')
         const initial = firstWord.charAt(0) + secondWord.charAt(0)
         // console.log(initial);
 
         contain = contain + `
             <div class="project">
                 <div class="oute">
-                    <div class="card">
+                    <div class="card" style="${name.img}">
                         <div class="big-text" >${initial}</div>
                     </div>
                 </div>
-                <div class="title-card">${name}</div>
+                <div class="title-card">${name.name}</div>
             </div>
         `
     } else if(i == 6){
@@ -116,7 +134,8 @@ $(document).on('input', '.search', (e)=>{
     let res = []
     taskz.map(task=>{
         if(task.name.toLocaleLowerCase().includes(text)){
-            res.push(task.name)
+            let data = {name:task.name, img:task.img}
+            res.push(data)
         }
     })
 
@@ -142,18 +161,18 @@ $(document).on('input', '.search', (e)=>{
 
 
             // get first letter on first word and second word
-            const [firstWord, secondWord] = name.split(' ')
+            const [firstWord, secondWord] = name.name.split(' ')
             const initial = firstWord.charAt(0) + secondWord.charAt(0)
             // console.log(initial);
     
             contain = contain + `
                 <div class="project">
                     <div class="oute">
-                        <div class="card">
+                        <div class="card" style="${name.img}">
                             <div class="big-text" >${initial}</div>
                         </div>
                     </div>
-                    <div class="title-card">${name}</div>
+                    <div class="title-card">${name.name}</div>
                 </div>
             `
         } else if(i == 6){
